01281d8100e41cb8c26504f74a9c4cb2947ef0ef
lucianciu <lucian.ciuciu@gmail.com>
2020-07-07 23:46:20 +0300

    First real commit - Updated Footer.vue

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,21 +1,21 @@
 <template>
-  <Layout>
-    <div class="d-flex">
-    <img class="" src="../assets/images/404.svg" />
-    </div>
-  </Layout>
+	<Layout>
+		<div class="d-flex">
+			<img class="" src="../assets/images/404.svg"/>
+		</div>
+	</Layout>
 </template>
 
 <style scoped lang="scss">
-img {
-  width: 80%;
-  margin: auto;
+	img {
+		width: 80%;
+		margin: auto;
 
-  @media (max-width: 500px) {
-    width: 90%;
-  }
-  @media (max-width: 400px) {
-    width: 100%;
-  }
-}
+		@media (max-width: 500px) {
+			width: 90%;
+		}
+		@media (max-width: 400px) {
+			width: 100%;
+		}
+	}
 </style>
