750d1fee4878d26b460dd6312b819dafeedac471
lucianciu <lucian.ciuciu@gmail.com>
2020-07-09 02:19:06 +0300

    Updated package.json, BlogContent.vue

--- src/components/BlogContent.vue
+++ src/components/BlogContent.vue
@@ -10,17 +10,21 @@
 	}
 </script>
 
-<style lang="sass" scoped>
-	p
-		line-height: 1.5
-		font-size: 1.15rem
+<style lang="scss" scoped>
+	p {
+		line-height: 1.5;
+		font-size: 1.15rem;
+	}
 
-	h2
-		font-size: 2rem
+	h2 {
+		font-size: 2rem;
+	}
 
-	h3
-		font-size: 1.5rem
+	h3 {
+		font-size: 1.5rem;
+	}
 
-	h4, h5, h6
-		font-size: 1.15rem
+	h4, h5, h6 {
+		font-size: 1.15rem;
+	}
 </style>
